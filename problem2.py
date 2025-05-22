7e9fa5ae30b66a060238f4f1713aa422952bae8d
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-22 11:17:20 +0600

    added python basic problem 2

---
+++ problem2.py
@@ -0,0 +1,15 @@
+# Input: "madam"
+# Output: True
+
+def is_palindrome(s):
+   
+    start= 0
+    end = len(s) - 1
+    while start<end:
+        if s[start] != s[end]:
+            return False
+        start += 1
+        end -= 1
+    return True
+# Test the function
+print(is_palindrome("madam"))  # Output: True
