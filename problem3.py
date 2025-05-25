2f22d0115b06e01eb89c0f85d94e134216a1b997
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-25 07:52:01 +0600

    added problem3

---
+++ problem3.py
@@ -0,0 +1,14 @@
+# Problem 3. Character frequency counter
+# Input: "programming"
+# Output: {'p': 1, 'r': 2, 'o': 1, 'g': 2, 'a': 1, 'm': 2, 'i': 1, 'n': 1}
+
+def char_frequency(s):
+    freq = {}
+    for char in s:
+        if char in freq:
+            freq[char] += 1
+        else:
+            freq[char] = 1
+    return freq
+# Test the function
+print(char_frequency("programming"))  # Output: {'p': 1, 'r': 2, 'o': 1, 'g': 2, 'a': 1, 'm': 2, 'i': 1, 'n': 1}
