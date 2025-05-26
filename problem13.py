3a979694c800edb880a3bd06959562e6409f69ac
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-26 16:47:22 +0600

    added problem 13

---
+++ problem13.py
@@ -0,0 +1,21 @@
+# find 2nd maximum number in the list without sort
+
+def second_maximum(nums):
+    if len(nums) < 2:
+        return None  # Second max possible na
+
+    max1 = float('-inf')
+    max2 = float('-inf')
+
+    for num in nums:
+        if num > max1:
+            max2 = max1
+            max1 = num
+        elif num > max2 and num != max1:
+            max2 = num
+
+    return max2 if max2 != float('-inf') else None
+
+# Test
+nums = [2, 6, 3, 1, 22, 34]
+print("Second Maximum:", second_maximum(nums))
